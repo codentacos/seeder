ee84808aa4e50ecd5d8d9d0edfc43297f667316d
Zachary Nelson <nelsonz2013@hotmail.com>
2020-09-12 16:28:23 -0500

    .env added. MongoDB is connected and data renders from the database now.

--- client/src/components/AppCard/AppCard.jsx
+++ client/src/components/AppCard/AppCard.jsx
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import style from './appCard.css';
 
-const AppCard = () => (
-  <Container className={style.appCard}>
-    <Row>
-      <Col xs={12}>
-        <h2>Droppr App</h2>
-      </Col>
-      <Col className="mb-2">
-        <span><u>Developed by: Jeff Stanz</u></span>
-      </Col>
-    </Row>
-    <Row>
-      <p className="ml-3 mr-3">
-        Consequat incididunt voluptate do tempor non incididunt adipisicing. Cillum laboris deserunt
-        elit irure aute aute. In pariatur ut ullamco qui est anim ad ullamco excepteur nisi culpa dolore
-        labore. Velit exercitation excepteur non eu eiusmod consectetur ullamco aliqua exercitation
-        incididunt tempor id minim. Aute mollit dolor reprehenderit est do ut sunt non mollit fugiat
-        esse in. Laboris tempor laboris deserunt cillum velit velit enim. Exercitation veniam aliqua
-        veniam irure incididunt do.
-      </p>
-    </Row>
-    <Row>
-      <Col>
-        <div className={`${style.heart} d-inline-block`}><span className="ml-4">222</span></div>
-      </Col>
-    </Row>
-  </Container>
-);
-
+const AppCard = (props) => {
+  const {desc} = props.appData;
+  const [description, setDescription] = useState(desc.length > 150 ? desc.slice(0, 150) : desc);
+  return (
+    <Container className={style.appCard}>
+      <Row>
+        <Col xs={12}>
+          <h2>{props.appData.appName}</h2>
+        </Col>
+        <Col className="mb-2">
+          <span><u>Developed by: {props.appData.dev}</u></span>
+        </Col>
+      </Row>
+      <Row>
+        <p className="ml-3 mr-3 mb-1">{`${description}...`}</p>
+        <span className='ml-3 mb-2'>See More</span>
+      </Row>
+      <Row>
+        <Col>
+          <div className={`${style.heart} d-inline-block`}><span className="ml-4">{props.appData.likes}</span></div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 export default AppCard;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const UserApp = require('../database/db.js');
+// const db = require('../database/db.js');
 
 app.use(express.static('public'));
 
+app.get('/home', (req, res) => {
+  UserApp.find({})
+    .then((response) => {
+      res.send(response);
+    });
+});
+
 app.listen(port, () => {
   console.log('App is running on port: ' + port);
-})
+});
