30a5c26510e265853c99fb1f871e8d9d8095f874
Zachary Nelson <nelsonz2013@hotmail.com>
2020-09-13 20:28:29 -0500

    updated prop names. Added conditional rendering for AppCard description length

--- client/src/components/AppCard/AppCard.jsx
+++ client/src/components/AppCard/AppCard.jsx
@@ -4,34 +4,43 @@
 /* eslint-disable object-curly-newline */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import style from './appCard.css';
 
 const AppCard = (props) => {
-  const { appData: { desc, appName, dev, likes, _id } } = props;
+  const { appData: { appDescription, appDev, appName, appLink, appLikes, _id } } = props;
   const { handleHearts } = props;
-  const [description, setDescription] = useState(desc.length > 150 ? desc.slice(0, 150) : desc);
+  const [description, setDescription] = useState(appDescription.length > 150 ? `${appDescription.slice(0, 150)}...` : appDescription);
   const [voted, setVoted] = useState(false);
+
+  const handleDescription = () => {
+    setDescription(description === appDescription ? `${appDescription.slice(0, 150)}...` : appDescription);
+  };
 
   return (
     <Container className={style.appCard}>
       <Row>
-        <Col xs={12}>
+        <Col xs={6}>
           <h2>{appName}</h2>
         </Col>
+        <Col xs={6} className="d-flex align-items-center justify-content-end">
+          <a href={`http://${appLink}`} target="_blank" rel="noreferrer"><Button size="sm" className={style.button}>Go To App</Button></a>
+        </Col>
+      </Row>
+      <Row>
         <Col className="mb-2">
           <span>
-            <u>Developed by: {dev}</u>
+            <u>Developed by: {appDev}</u>
           </span>
         </Col>
       </Row>
       <Row>
-        <p className="ml-3 mr-3 mb-1">{`${description}...`}</p>
-        <span className="ml-3 mb-2">See More</span>
+        <p className="ml-3 mr-3 mb-1">{`${description}`}</p>
+        {description.length > 150 ? <span className={`ml-3 mb-2 ${style.seeMoreButton}`} onClick={handleDescription}>See More</span> : null}
       </Row>
       <Row>
         <Col>
-          <div className={voted === false ? `${style.heart} d-inline-block` : `${style.heartFull} d-inline-block`} onClick={voted === false ? () => { handleHearts(_id); setVoted(true); } : null}><span className="ml-4">{likes}</span></div>
+          <div className={voted === false ? `${style.heart} d-inline-block` : `${style.heartFull} d-inline-block`} onClick={voted === false ? () => { handleHearts(_id); setVoted(true); } : null}><span className="ml-4">{appLikes}</span></div>
         </Col>
       </Row>
     </Container>
@@ -42,9 +51,10 @@ AppCard.propTypes = {
   handleHearts: PropTypes.func.isRequired,
   appData: PropTypes.shape({
     appName: PropTypes.string,
-    desc: PropTypes.string,
-    dev: PropTypes.string,
-    likes: PropTypes.number,
+    appDescription: PropTypes.string,
+    appDev: PropTypes.string,
+    appLink: PropTypes.string,
+    appLikes: PropTypes.number,
     _id: PropTypes.string,
   }).isRequired,
 };
