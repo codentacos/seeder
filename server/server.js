e9a9168c54b9a658465928ccdd2fa838f5edf824
Zachary Nelson <nelsonz2013@hotmail.com>
2020-10-10 14:47:39 -0500

    added auth dependencies. Migrated routes from server file to routes file

--- server/server.js
+++ server/server.js
@@ -1,46 +1,31 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+// require('../auth/config/passport.js');
 
 const app = express();
 const port = 3000;
+const passport = require('passport');
 const UserApp = require('../database/db.js');
 
+// express app setup
+app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// /home gets initial data on homepage load
-app.get('/home', (req, res) => {
-  UserApp.find({})
-    .sort({ appLikes: 'desc' })
-    .then((response) => {
-      res.send(response);
-    });
-});
+// required for passport
+app.use(session({ secret: 'thisismysessionsecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messeges stored in the session
 
-// catch all redirect for refreshing on other routes
-app.get('/*', (req, res) => {
-  res.redirect('/');
-});
-
-// /like updates record in database for liking applications
-app.put('/like', (req, res) => {
-  UserApp.findByIdAndUpdate(`${req.body.id}`, { $inc: { appLikes: 1 } }, (err, response) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.sendStatus(204);
-    }
-  });
-});
-
-// /submitapp will insert a newly submitted app to the database
-app.post('/submitapp', (req, res) => {
-  console.log(req.body);
-  UserApp.create(req.body);
-  res.sendStatus(201);
-});
+require('../client/src/routes.js')(app, passport);
 
 app.listen(process.env.PORT || port, () => {
   console.log(`App is running on port: ${port}`);
